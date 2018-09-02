0eb8adab614b91c9e74f0496f4f11ac07ef362bb
Altaf B <altafb2010@gmail.com>
2018-09-02 03:05:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array 
   }
   
-function addElementToEndOfArray(array, element) {choco = [...element, array]
+function addElementToEndOfArray(array, element) {[...array, element]
 return choco }
 
 function destructivelyAddElementToEndOfArray(array, element) {array.push(element)
