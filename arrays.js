e3b5edfb52fb29906d8b814b83d8afa8a1722daf
Altaf B <altafb2010@gmail.com>
2018-09-02 02:22:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,9 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array 
   }
 function addElementToEndOfArray(array, element) {return [...element, array]}
-function destructivelyAddElementToEndOfArray(array, element) {return [array.push(element)]}
+function destructivelyAddElementToEndOfArray(array, element) 
+{ array.push(element) return array}
+
 function accessElementInArray(array, index) {return (array[index])}
 function destructivelyRemoveElementFromBeginningOfArray(array, element) {return [array.shift(element)]}
 function removeElementFromBeginningOfArray(array) {return [array.slice(1)]}
