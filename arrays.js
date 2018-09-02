0dcedbb3cdf5a6d6bd86fe721163dfdaad2f7147
Altaf B <altafb2010@gmail.com>
2018-09-02 02:49:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ return array
 function destructivelyRemoveElementFromEndOfArray(array) {return [array.pop()]}
 function removeElementFromEndOfArray(array) 
 { 
-  array.slice(0, array.length -1) 
+  array.slice(0, array.length - 1) 
   return array 
   
 }
