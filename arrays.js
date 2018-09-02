957aaac190329fc263cc9386a3bb3e7e1d3d9f4e
Altaf B <altafb2010@gmail.com>
2018-09-02 02:17:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array, element) {return [element, ...array] }
-function destructivelyAddElementToBeginningOfArray(array, element) { array.unshift(element) return array }
+function destructivelyAddElementToBeginningOfArray(array, element)  array.unshift(element) {return array }
 function addElementToEndOfArray(array, element) {return [...element, array]}
 function destructivelyAddElementToEndOfArray(array, element) {return [array.push(element)]}
 function accessElementInArray(array, index) {return (array[index])}
