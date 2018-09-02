97e70573f7264d8597b5c21bce1eb2e8b2bddc46
Altaf B <altafb2010@gmail.com>
2018-09-02 02:46:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element)
 return array}
 
 function removeElementFromBeginningOfArray(array) 
-{array.slice(0) 
+{array.slice(1) 
 return array
   
 }
