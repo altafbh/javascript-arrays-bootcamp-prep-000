716bdd7797dac2201a66e750ba38addaacc4f298
Altaf B <altafb2010@gmail.com>
2018-09-02 02:40:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,6 +19,10 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element)
 {array.shift(element) 
 return array}
 
-function removeElementFromBeginningOfArray(array) {return [array.slice(1)]}
+function removeElementFromBeginningOfArray(array) {array.slice(1) 
+return array
+  
+}
+
 function destructivelyRemoveElementFromEndOfArray(array) {return [array.pop()]}
 function removeElementFromEndOfArray(array) {return [array.slice(0, array.length -1)]}
